9d83d843e0f46047ce3465525e0190e32c2066d0
gibber9809 <gibber9809@users.noreply.github.com>
2017-06-01 14:23:49 -0400

    Add button to delete people from database

--- app/src/main/java/com/example/peopleloader/PeopleViewerActivity.java
+++ app/src/main/java/com/example/peopleloader/PeopleViewerActivity.java
@@ -1,22 +1,31 @@
 package com.example.peopleloader;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PeopleViewerActivity extends AppCompatActivity {
     @BindView (R.id.people_pager) protected ViewPager mPeoplePager;
     private PageAdapter mAdapter;
     private DatabaseHelper mDatabaseHelper;
+
+    private int mResumePosition = 0;
+
+    private DeletePersonAsync mDelete = null;
+    private LoadCursorAsync mLoad = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +36,54 @@ public class PeopleViewerActivity extends AppCompatActivity {
         mDatabaseHelper = new DatabaseHelper(this);
         mAdapter = new PageAdapter(getSupportFragmentManager());
 
-        LoadCursorAsync loader = new LoadCursorAsync();
-        loader.execute(mDatabaseHelper);
+        mLoad = new LoadCursorAsync();
+        mLoad.execute(mDatabaseHelper);
 
         mPeoplePager.setAdapter(mAdapter);
 
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (NullPointerException e) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-    private class PageAdapter extends FragmentPagerAdapter {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                upIntent.putExtra(LoadActivity.DB_STATUS_EXTRA, true);
+                NavUtils.navigateUpTo(this, upIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @OnClick(R.id.search_person_button)
+    public void searchCurrentPerson() {
+        if (mDelete != null) {
+            String name = mAdapter.getPersonName(mPeoplePager.getCurrentItem());
+        }
+    }
+
+    @OnClick(R.id.delete_button)
+    public void deleteCurrentPerson() {
+        if (mDelete == null) {
+            int currentPosition = mPeoplePager.getCurrentItem();
+            mDelete = new DeletePersonAsync(mAdapter.getPersonName(currentPosition));
+            if (currentPosition > 0) {
+                mResumePosition = currentPosition - 1;
+            } else if (mAdapter.getCount() > 1) {
+                mResumePosition = currentPosition;
+            } // Last case is that this is the only remaining item
+
+            mDelete.execute(mDatabaseHelper);
+        }
+    }
+
+    private class PageAdapter extends FragmentStatePagerAdapter {
         private Cursor mPeopleCursor = null;
 
         public PageAdapter(FragmentManager fm) {
@@ -65,6 +114,12 @@ public class PeopleViewerActivity extends AppCompatActivity {
         public void setPeopleCursor(Cursor cursor) {
             mPeopleCursor = cursor;
         }
+
+        public String getPersonName(int position) {
+            mPeopleCursor.moveToPosition(position);
+            return mPeopleCursor
+                    .getString(mPeopleCursor.getColumnIndex(DatabaseHelper.COLUMN_PERSON));
+        }
     }
 
 
@@ -72,6 +127,7 @@ public class PeopleViewerActivity extends AppCompatActivity {
      * Subclass of (@link AsyncTask) to load a cursor to our database
      */
     private class LoadCursorAsync extends AsyncTask<DatabaseHelper,Void,Cursor> {
+
         @Override
         protected Cursor doInBackground(DatabaseHelper... params) {
             if (params.length > 0 ) {
@@ -84,17 +140,61 @@ public class PeopleViewerActivity extends AppCompatActivity {
                         null, null, null, null, DatabaseHelper.COLUMN_PERSON);
 
                 //Perform an operation to force cursor to load now
-                cursor.getCount();
-                return cursor;
+                if (cursor.getCount() > 0)
+                    return cursor;
+                else
+                    return null;
             }
             return null;
         }
 
         @Override
         protected void onPostExecute(Cursor cursor) {
-            mAdapter.setPeopleCursor(cursor);
-            mAdapter.notifyDataSetChanged();
+            if (cursor != null) {
+                mAdapter.setPeopleCursor(cursor);
+                mAdapter.notifyDataSetChanged();
+                mPeoplePager.setCurrentItem(mResumePosition, false);
+            } else {
+                Intent upIntent = NavUtils.getParentActivityIntent(PeopleViewerActivity.this);
+                upIntent.putExtra(LoadActivity.DB_STATUS_EXTRA, false);
+                NavUtils.navigateUpTo(PeopleViewerActivity.this, upIntent);
+            }
+            mLoad = null;
         }
+    }
+
+    private class DeletePersonAsync extends AsyncTask<DatabaseHelper,Void,Void> {
+        private String mName;
+
+        public DeletePersonAsync(String name) {
+            mName = name;
+        }
+
+        @Override
+        public void onPreExecute() {
+            mPeoplePager.setAdapter(null);
+            mAdapter = new PageAdapter(getSupportFragmentManager());
+        }
+
+        @Override
+        protected Void doInBackground(DatabaseHelper... params) {
+            if (params.length > 0) {
+                SQLiteDatabase db = params[0].getWritableDatabase();
+                db.delete(DatabaseHelper.TABLE,
+                        getString(R.string.delete_format, DatabaseHelper.COLUMN_PERSON, mName),
+                        null);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void ignore) {
+            mPeoplePager.setAdapter(mAdapter);
+            mLoad = new LoadCursorAsync();
+            mLoad.execute(mDatabaseHelper);
+            mDelete = null;
+        }
+
     }
 
 }
